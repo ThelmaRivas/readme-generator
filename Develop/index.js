2fee851d730d3fe3615d4afb5922fa1ce1dcecba
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-13 14:56:57 -0600

    Create prompt questions
    create the questions array that the command prompt is going to ask the user

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information.'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please provide contribution guidelines.'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide test instructions.'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license for your project',
+            choices: ['MIT', 'Apache', 'GPL', 'None']
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Please provide any additional information orquestions:'
+
+        },
+    ])
+
